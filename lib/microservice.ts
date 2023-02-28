232ccc2a4b5bee790bfb48e2766687d54386e174
jjchavarrg <jjchavarrg@eafit.edu.co>
2023-02-27 22:24:05 -0500

    feat(basket): develop checkout basket for send event
    develop checkout basket for send event to AWS EventBridge from Basket
    with SDK

--- lib/microservice.ts
+++ lib/microservice.ts
@@ -66,6 +66,9 @@ export class SwnMicroservices extends Construct {
       environment: {
         PRIMARY_KEY: "userName",
         DYNAMODB_TABLE_NAME: basketTable.tableName,
+        EVENT_SOURCE: "com.swn.basket.checkoutbasket",
+        EVENT_DETAILTYPE: "CheckoutBasket",
+        EVENT_BUSNAME: "SwnEventBus",
       },
       runtime: Runtime.NODEJS_14_X,
     };
