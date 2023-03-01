2ce521c5a21a7240c7a203c6978e8602653b5141
jjchavarrg <jjchavarrg@eafit.edu.co>
2023-02-28 22:34:06 -0500

    feat(ordering): developing ordering microservice sync invocation
    developing ordering microservice sync invocation from api gateway
    
    - developing ordering lambda microservice sync invocation from api gateway
    - developing getOrder method
    - developing getAllOrders method
    - integration with DynamoDB client

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -1,6 +1,10 @@
 // Import required AWS SDK clients and commands for Node.js
-import { PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { marshall } from "@aws-sdk/util-dynamodb";
+import {
+  PutItemCommand,
+  QueryCommand,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { ddbClient } from "./ddbClient.js";
 
 exports.handler = async function (event) {
@@ -25,10 +29,6 @@ const eventBridgeInvocation = async (event) => {
   await createOrder(event.detail);
 };
 
-const apiGatewayInvocation = async (event) => {
-  // Implement this
-};
-
 const createOrder = async (basketCheckoutEvent) => {
   console.log(`createOrder function.event : "${basketCheckoutEvent}"`);
 
@@ -47,6 +47,91 @@ const createOrder = async (basketCheckoutEvent) => {
     console.log("Success", createResult);
 
     return createResult;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const apiGatewayInvocation = async (event) => {
+  // GET /order
+  // GET /order/{userName}
+
+  let body;
+
+  try {
+    switch (event.httpMethod) {
+      case "GET":
+        if (event.pathParameters != null) {
+          body = await getOrder(event); // GET /order/{userName}
+        } else {
+          body = await getAllOrders(); // GET /order
+        }
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.httpMethod}"`);
+    }
+
+    console.log("body: ", body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Successfully finished operation ${event.httpMethod}`,
+        body: body,
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Failed to perfom operation",
+        errorMsg: err.message,
+        errorStack: err.errorStack,
+      }),
+    };
+  }
+};
+
+const getOrder = async (event) => {
+  console.log("getOrder");
+
+  try {
+    // expected request : xxx/order/swn?orderDate=timestamp
+    const userName = event.pathParameters.userName;
+    const orderDate = event.queryStringParameters.orderDate;
+
+    const params = {
+      KeyConditionExpression: "userName = :userName and orderDate = :orderDate",
+      ExpressionAttributeValues: {
+        ":userName": { S: userName },
+        ":orderDate": { S: orderDate },
+      },
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+    };
+
+    const { Items } = await ddbClient.send(new QueryCommand(params));
+    console.log("Success", Items);
+
+    return Items ? Items.map((Item) => unmarshall(Item)) : {};
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const getAllOrders = async () => {
+  console.log("getAllOrders");
+
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+    };
+
+    const { Items } = await ddbClient.send(new ScanCommand(params));
+    console.log("Success", Items);
+
+    return Items ? Items.map((Item) => unmarshall(Item)) : {};
   } catch (err) {
     console.error(err);
     throw err;
