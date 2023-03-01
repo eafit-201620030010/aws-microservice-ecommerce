3d8ab809aae0c1fae61892433b7054152daa90fe
jjchavarrg <jjchavarrg@eafit.edu.co>
2023-02-28 21:36:23 -0500

    feat(ordering): develop ordering microservice consume checkout event
    Develop ordering microservice consume checkout event form aws
    eventBridge with sdk

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -1,8 +1,54 @@
+// Import required AWS SDK clients and commands for Node.js
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { ddbClient } from "./ddbClient.js";
+
 exports.handler = async function (event) {
   console.log("request:", JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello from Ordering! You've hit ${event.path}\n`,
-  };
+
+  // TODO - Catch and Process Async EventBridge Invocation and Sync API Gateway Invocation
+
+  const eventType = event["detail-type"];
+  if (eventType !== undefined) {
+    // EventBridge Invocation
+    await eventBridgeInvocation(event);
+  } else {
+    // API Gateway Invocation -- return sync response
+    return await apiGatewayInvocation(event);
+  }
+};
+
+const eventBridgeInvocation = async (event) => {
+  console.log(`eventBridgeInvocation function. event : "${event}"`);
+
+  // create order item into db
+  await createOrder(event.detail);
+};
+
+const apiGatewayInvocation = async (event) => {
+  // Implement this
+};
+
+const createOrder = async (basketCheckoutEvent) => {
+  console.log(`createOrder function.event : "${basketCheckoutEvent}"`);
+
+  try {
+    // set orderDate for SK of order dynamodb
+    const orderDate = new Date().toISOString();
+    basketCheckoutEvent.orderDate = orderDate;
+    console.log(basketCheckoutEvent);
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(basketCheckoutEvent || {}),
+    };
+
+    const createResult = await ddbClient.send(new PutItemCommand(params));
+    console.log("Success", createResult);
+
+    return createResult;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
